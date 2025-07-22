6979e2c208a87542a641ab8260b38a6c480bc14d
SebastianDlaCruz <sebastiandelacruz335@linktic.com>
2025-07-22 11:40:11 -0300

    agregando patron method factory

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { urlBaseInterceptor } from './core/interceptors/url-base/url-base.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideHttpClient(withFetch()), [urlBaseInterceptor]]
 };
